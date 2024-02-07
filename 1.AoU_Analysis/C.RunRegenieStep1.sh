09970cd177479485e19510a8800282a42e64f0fe
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-02-07 12:11:30 -0500

    Update C.RunRegenieStep1.sh

--- 1.AoU_Analysis/C.RunRegenieStep1.sh
+++ 1.AoU_Analysis/C.RunRegenieStep1.sh
@@ -2,16 +2,18 @@
 awk 'NR==1 {print "#FID\tIID\tSEX"} NR>1 {print "0\t" $1 "\t" "NA"}' array_data/arrays_autosomes_post_qc_pruned_common.psam > tmp ;\
 mv tmp array_data/arrays_autosomes_post_qc_pruned_common.psam ;\
 
+groups=(hisp non_hisp)
 ../regenie_v3.2.8.gz_x86_64_Linux \
     --step 1 \
     --pgen array_data/arrays_autosomes_post_qc_pruned_common \
     --phenoFile ../regenie_pheno.txt \
     --covarFile ../regenie_covar_20pcs.txt \
     --bt \
-    --out rg_step1/rg_step1_hisp \
+    --out rg_step1/rg_step1_${groups[1]} \
     --bsize 1000 \
     --lowmem \
-    --lowmem-prefix tmp_rg_20_hisp \
+    --lowmem-prefix tmp_rg_20_${groups[1]} \
     --phenoCol AD_any \
-    --keep rg_input/hisp_ids_3sd.txt
+    --keep rg_input/${groups[1]}_ids_3sd.txt
 # 1695 cases and 54767 controls for HISP with 3 SDs
+# 'AD_any': 9595 cases and 178680 controls for NON HISP with 3 SDs
