e08c62fd7b11d4597664fffd6487cc354f10075c
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-02-07 12:09:04 -0500

    Update D.RunRegenieStep2.sh

--- 1.AoU_Analysis/D.RunRegenieStep2.sh
+++ 1.AoU_Analysis/D.RunRegenieStep2.sh
@@ -1,4 +1,4 @@
-for ((chr=1;chr<=22;chr++)); do \
+efor ((chr=1;chr<=22;chr++)); do \
                 ../regenie_v3.2.8.gz_x86_64_Linux \
                     --step 2 \
                     --pgen ../pgen_geno_1e-1_mac_20/chr${chr} \
@@ -12,3 +12,5 @@ for ((chr=1;chr<=22;chr++)); do \
                     --minMAC 20 --keep rg_input/hisp_ids_3sd.txt \
                     --phenoCol AD_any ;\
 done
+bucket="gs://fc-secure-33ab7182-9bff-4fee-bf6d-9e3b5f072033" # the Hisp/AMR study workspace
+gsutil -m cp -rn rg_step2/* $bucket/data/rg_results/
