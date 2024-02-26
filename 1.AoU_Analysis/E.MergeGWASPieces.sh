1947bf461c9176b85794cd1b3812abf35d7d56fd
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-02-26 16:53:36 -0500

    Update E.MergeGWASPieces.sh

--- 1.AoU_Analysis/E.MergeGWASPieces.sh
+++ 1.AoU_Analysis/E.MergeGWASPieces.sh
@@ -1,8 +1,13 @@
-groups=(hisp non_hisp) ;\
-for grp in "${groups[@]}"; do \
+#groups=(hisp non_hisp) ;\
+#for grp in "${groups[@]}"; do \
     echo $grp ;\
-    head -n 1 chr9_${grp}_multi_geno_1e-1_mac_20_commonpcs_AD_any_C.regenie > aou_AD_any_grp_${grp}_gwas.txt ;\
-    for file in *${grp}*.regenie; do \
-        tail -n +2 "$file" >> aou_AD_any_grp_${grp}_gwas.txt ;\
-    done \
-done
+    head -n 1 chr9_hisp_multi_geno_1e-1_mac_20_commonpcs_AD_any_C.regenie > aou_AD_any_grp_hisp_gwas.txt ;\
+    head -n 1 chr9_non_hisp_multi_geno_1e-1_mac_20_commonpcs_AD_any_C.regenie > aou_AD_any_grp_non_hisp_gwas.txt ;\
+    for file in *regenie; do \
+        if echo "$file" | grep -q "$non_hisp"; then \
+            tail -n +2 "$file" >> aou_AD_any_grp_non_hisp_gwas.txt ;\
+        else \
+            tail -n +2 "$file" >> aou_AD_any_grp_hisp_gwas.txt ;\
+        fi \
+    done 
+#done
