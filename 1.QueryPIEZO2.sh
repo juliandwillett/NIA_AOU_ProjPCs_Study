4bdc831584c3acde5628a5c5fee4950690f84ad1
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-02-29 14:40:44 -0500

    Update 1.QueryPIEZO2.sh

--- 1.QueryPIEZO2.sh
+++ 1.QueryPIEZO2.sh
@@ -10,8 +10,18 @@
   --out /n/home09/jwillett/true_lab_storage/02_PIEZO2_HISP/piezo2_ld --ld-window-r2 0
 
 #######
-# Get info for fine mapping
+# Get info for fine mapping: 500 kb window
+# Get all variants in window, no p value filter
+zcat /n/home09/jwillett/true_lab_storage/Data_Links/NIAGADS_Personal/selfHISP8467_a_pc_5JPCs_ss.Affection.Status.glm.logistic.full.dn8.gz | \
+awk 'NR==1 || ($1 == 18 && $2 >= 10894647 && $2 < 11394647 && $8 != "NA" && $9 != "NA") {print}' > \
+/n/home09/jwillett/true_lab_storage/Data_Links/NIAGADS_Personal/selfHISP8467_a_pc_5JPCs_ss.Affection.Status.glm.logistic.full.piezo2.txt
+
+# Get the IDs
+awk 'NR>1 && $8 != "NA" && $9 != "NA" {print $3}' \
+/n/home09/jwillett/true_lab_storage/Data_Links/NIAGADS_Personal/selfHISP8467_a_pc_5JPCs_ss.Affection.Status.glm.logistic.full.piezo2.txt > \
+/n/home09/jwillett/true_lab_storage/Data_Links/NIAGADS_Personal/selfHISP8467_a_pc_5JPCs_ss.Affection.Status.glm.logistic.full.piezo2_just_ids.txt
+
+# Get the R matrix for susieR
 ./plink2 --pfile /n/home09/jwillett/true_lab_storage/Data_Links/NIAGADS_Personal/NIAGADS_PIEZO2_PLINK \
-  --r-phased square \
-  --out /n/home09/jwillett/true_lab_storage/02_PIEZO2_HISP/piezo2_finemap_data --chr 18 \
-  --from-bp 11139700 --to-bp 11159498
+  --r-phased square --extract /n/home09/jwillett/true_lab_storage/Data_Links/NIAGADS_Personal/selfHISP8467_a_pc_5JPCs_ss.Affection.Status.glm.logistic.full.piezo2_just_ids.txt \
+  --out /n/home09/jwillett/true_lab_storage/02_PIEZO2_HISP/piezo2_finemap_data
