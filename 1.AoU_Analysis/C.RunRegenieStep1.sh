99260a82fe889d43317719f5e836f6445c132ac3
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-03-03 15:09:45 -0500

    Update C.RunRegenieStep1.sh

--- 1.AoU_Analysis/C.RunRegenieStep1.sh
+++ 1.AoU_Analysis/C.RunRegenieStep1.sh
@@ -2,7 +2,7 @@
 awk 'NR==1 {print "#FID\tIID\tSEX"} NR>1 {print "0\t" $1 "\t" "NA"}' array_data/arrays_autosomes_post_qc_pruned_common.psam > tmp ;\
 mv tmp array_data/arrays_autosomes_post_qc_pruned_common.psam ;\
 
-groups=(hisp non_hisp)
+groups=(hisp)
 ../regenie_v3.2.8.gz_x86_64_Linux \
     --step 1 \
     --pgen array_data/arrays_autosomes_post_qc_pruned_common \
@@ -13,8 +13,8 @@ groups=(hisp non_hisp)
     --bsize 1000 \
     --lowmem \
     --lowmem-prefix tmp_rg_20_${groups[0]} \
-    --phenoCol AD,AD_any \
-    --keep rg_input/${groups[0]}_ids_3sd.txt
+    --phenoCol AD_any \
+    --keep hisp_3sd_ids.txt
 # 1695 cases and 54767 controls for HISP with 3 SDs
 # 'AD_any': 9595 cases and 178680 controls for NON HISP with 3 SDs
 # Only 38 ICD AD cases for HISP, so not done.
