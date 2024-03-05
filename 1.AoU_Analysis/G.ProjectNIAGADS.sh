6ad1b4aab3c0e425d9ca1b6aedc926e2fe142501
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-03-05 17:37:35 -0500

    Update G.ProjectNIAGADS.sh

--- 1.AoU_Analysis/G.ProjectNIAGADS.sh
+++ 1.AoU_Analysis/G.ProjectNIAGADS.sh
@@ -1,8 +1,9 @@
 # Move PIEZO2 pgen files from FASRC onto AoU: piezo2_work/NIAGADS/NIAGADS_PIEZO2_PLINK
 
 # Take array data for projection, isolate HISP folks
-./plink2 --pfile piezo2_work/array_data/arrays_autosomes_post_qc_pruned_common --keep piezo2_work/rg_input/hisp_ids_3sd.txt \
-   --make-bed --out piezo2_work/NIAGADS/aou_hisp_pruned --set-all-var-ids @:#:\$r:\$a --new-id-max-allele-len 10000 
+./plink2 --pfile piezo2_work/array_data/arrays_allchr --keep piezo2_work/rg_input/hisp_ids_3sd.txt --maf 0.01 \
+   --geno 0.1 --hwe 1e-15 --chr 1-22 \
+   --make-pgen --out piezo2_work/NIAGADS/aou_hisp --set-all-var-ids @:#:\$r:\$a --new-id-max-allele-len 10000 
 ./plink2 --bfile piezo2_work/NIAGADS/aou_hisp_pruned --extract piezo2_work/NIAGADS/shared_IDs_aou.txt \
    --make-bed --out piezo2_work/NIAGADS/aou_hisp_pruned_shared_hits
    
