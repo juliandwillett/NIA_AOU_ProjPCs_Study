0863c06827dcf07d8257ae2e55eef0a6dc54a37a
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-03-07 12:56:23 -0500

    Update G.ProjectNIAGADS.sh

--- 1.AoU_Analysis/G.ProjectNIAGADS.sh
+++ 1.AoU_Analysis/G.ProjectNIAGADS.sh
@@ -4,26 +4,17 @@
 ./plink2 --pfile piezo2_work/array_data/arrays_allchr --keep piezo2_work/rg_input/hisp_ids_3sd.txt --maf 0.01 \
    --geno 0.1 --hwe 1e-15 --chr 1-22 \
    --make-pgen --out piezo2_work/NIAGADS/aou_hisp --set-all-var-ids @:#:\$r:\$a --new-id-max-allele-len 10000 
-./plink2 --bfile piezo2_work/NIAGADS/aou_hisp_pruned --extract piezo2_work/NIAGADS/shared_IDs_aou.txt \
-   --make-bed --out piezo2_work/NIAGADS/aou_hisp_pruned_shared_hits
    
-# Make pruned dataset for NIAGADS - does not overlap with AoU after pruned
+# Convert to BED for R package, isolating shared hits to save memory/time
+./plink2 --pfile array_data/aou_hisp_array_maf_hwe_geno --extract piezo2_projection/shared_IDs_aou.txt \
+   --make-bed --out piezo2_projection/aou_array_nia_intersect
+./plink2 --pfile piezo2_projection/NIAGADS_hisp_maf_1e-2_variants --geno 0.1 --hwe 1e-15 --extract piezo2_projection/shared_IDs_nia.txt \
+   --chr 1-22 --make-bed --out piezo2_projection/nia_aou_intersect
+
+
 ./plink2 --pfile piezo2_work/NIAGADS/NIAGADS_hisp_maf_1e-2_variants --chr 1-22 --geno 0.1 \
   --make-bed --out piezo2_work/NIAGADS/NIAGADS_hisp_postqc --extract piezo2_work/NIAGADS/shared_IDs_nia.txt
 
-# Turn to bed format
-./plink2 --pfile piezo2_work/NIAGADS/NIAGADS_hisp_maf_1e-2_variants --geno 0.1 --make-bed --chr 1-22 \
-  --out piezo2_work/NIAGADS/NIAGADS_hisp_qc_noclump
-
 # Isolate locus
-./plink2 --pfile pgen_geno_1e-1_mac_20/chr18 --keep piezo2_work/rg_input/hisp_ids_3sd.txt \
-   --make-bed --out piezo2_work/NIAGADS/aou_hisp_piezo2 --chr 18 --from-bp 10644647 --to-bp 11644647
-
-# Run Rcode to find the mutual hits (CHRPOS intersection)
-
-# Use plink to isolate those hits
-./plink2 --pfile piezo2_work/NIAGADS/aou_hisp_piezo2 --extract piezo2_work/NIAGADS/shared_IDs.txt \
-  --make-bed --out piezo2_work/NIAGADS/aou_hisp_piezo2_nia_intersect
-./plink2 --pfile piezo2_work/NIAGADS/NIAGADS_PIEZO2_PLINK --make-bed --out piezo2_work/NIAGADS/NIAGADS_PIEZO2_PLINK
-
-## VERY GOOD OVERLAP
+#./plink2 --pfile pgen_geno_1e-1_mac_20/chr18 --keep piezo2_work/rg_input/hisp_ids_3sd.txt \
+#   --make-bed --out piezo2_work/NIAGADS/aou_hisp_piezo2 --chr 18 --from-bp 10644647 --to-bp 11644647
