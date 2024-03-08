5cdfc3f136b8554b392a1dd1eeb66ea990f01e62
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-03-08 13:42:00 -0500

    Update G.ProjectNIAGADS.sh

--- 1.AoU_Analysis/G.ProjectNIAGADS.sh
+++ 1.AoU_Analysis/G.ProjectNIAGADS.sh
@@ -1,6 +1,6 @@
 # Move PIEZO2 pgen files from FASRC onto AoU: piezo2_work/NIAGADS/NIAGADS_PIEZO2_PLINK
 
-# Take array data for projection, isolate HISP folks
+# Take array data for projection, isolate HISP folks for testing (use full data for actual analysis to maximize case count)
 ./plink2 --pfile array_data/arrays_allchr --geno 0.1 --hwe 1e-15 --chr 1-22 --maf 0.01 \
    --make-pgen --out array_data/aou_all_array_maf_hwe_geno --set-all-var-ids @:#:\$r:\$a \
    --new-id-max-allele-len 10000 --keep regenie_input/regenie_covar_fid_iid_only.txt
