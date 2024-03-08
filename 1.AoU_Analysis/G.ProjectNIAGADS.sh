46775fe2b7c8ee0f85abc52333160a72db97af73
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-03-08 13:42:09 -0500

    Update G.ProjectNIAGADS.sh

--- 1.AoU_Analysis/G.ProjectNIAGADS.sh
+++ 1.AoU_Analysis/G.ProjectNIAGADS.sh
@@ -13,11 +13,3 @@
    --make-bed --out piezo2_projection/aou_array_nia_intersect
 ./plink2 --pfile piezo2_projection/NIAGADS_hisp_maf_1e-2_variants --geno 0.1 --hwe 1e-15 --extract piezo2_projection/shared_IDs_nia.txt \
    --chr 1-22 --make-bed --out piezo2_projection/nia_aou_intersect
-
-
-./plink2 --pfile piezo2_work/NIAGADS/NIAGADS_hisp_maf_1e-2_variants --chr 1-22 --geno 0.1 \
-  --make-bed --out piezo2_work/NIAGADS/NIAGADS_hisp_postqc --extract piezo2_work/NIAGADS/shared_IDs_nia.txt
-
-# Isolate locus
-#./plink2 --pfile pgen_geno_1e-1_mac_20/chr18 --keep piezo2_work/rg_input/hisp_ids_3sd.txt \
-#   --make-bed --out piezo2_work/NIAGADS/aou_hisp_piezo2 --chr 18 --from-bp 10644647 --to-bp 11644647
