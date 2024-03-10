60a4801b8354026a10e7848576e31aaf8531c28a
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-03-10 16:42:58 -0400

    Create A.GetVariantsInLD.sh

---
+++ 2.NIAGADS_Analysis/A.GetVariantsInLD.sh
@@ -0,0 +1,4 @@
+# key variants: rs5018879, rs3925017, rs4573993, rs4413029
+./plink2 --pfile /n/home09/jwillett/true_lab_storage/Data_Links/NIAGADS_Personal/NIAGADS_PIEZO2_PLINK \
+  --r2-phased --ld-window-r2 0.9 --out /n/home09/jwillett/true_lab_storage/02_PIEZO2_HISP/piezo2_r2_phased_matrix \
+  --memory 120000
