b14afcc9119b07a1a5b4b4fb2fcb7fd163cb2539
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-03-14 14:57:13 -0400

    Update A.Get_LD_Data.sh

--- 3.UKB_Analysis/A.Get_LD_Data.sh
+++ 3.UKB_Analysis/A.Get_LD_Data.sh
@@ -4,7 +4,6 @@ wc -l no_missing_covar2 # right covar file for inclusion
 awk '{gsub(/\.[0]+$/, "", $1); gsub(/\.[0]+$/, "", $2); print $1 "\t" $2}' no_missing_covar2 > fid_iid_participants.txt
 
 ./plink2 --pfile merged_chromosome_18 --keep fid_iid_participants.txt \
-  --geno 0.1 \
+  --geno 0.1 --set-all-var-ids @-#-\$r-\$a --new-id-max-allele-len 10000 \
   --r2-phased --ld-window-r2 0.1 --chr 18 --from-bp 10644647 --to-bp 11644647 \
   --out ukb_all_piezo2_geno_1e-1_r2_1e-1 --memory 100000
-  
