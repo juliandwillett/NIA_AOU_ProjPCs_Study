9ac7f93581f965d309f3e4d54d9785343db8e197
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-04-01 16:02:02 -0400

    Update A.Meta_AoU_NIA.sh

--- 3.MetaAnalysis/A.Meta_AoU_NIA.sh
+++ 3.MetaAnalysis/A.Meta_AoU_NIA.sh
@@ -12,7 +12,8 @@ nia_hisp="/n/home09/jwillett/true_lab_storage/Data_Links/NIAGADS_Personal/selfHI
 
 awk 'NR==1 {$14 = "Pval"; print $0} NR>1 {$14 = 10^(-1 * $12); print $0}' ${aou_nia_genphen_match}.txt > ${aou_nia_genphen_match}_pval.txt
 zcat ${nia_hisp}.dn8.gz |\
-  awk 'NR==1 {$5 = "ALLELE1"; $6 = "ALLELE0"; $7 = "A1FREQ"; $8 = "BETA"; $9 = "SE"; $10 = "Pval"; print $0} NR>1 {print $0}' \
+  awk 'NR==1 {$3 = "ID"; $5 = "ALLELE1"; $6 = "ALLELE0"; $7 = "A1FREQ"; $8 = "BETA"; $9 = "SE"; $10 = "Pval"; print $0} \
+    NR>1 {gsub(":", "-", $3); print $0}' \
   > ${nia_hisp}_for_metal.txt
 
 ### Then run the meta analysis
@@ -37,4 +38,11 @@ salloc -p test --mem 80000 -t 0-02:00 -n 4
 /n/home13/dprokopenko/bin/metal METAL_script_AOU_NIAGADS.txt
 
 ### Then rename output for downstream work.
-mv 
+mv METAANALYSIS1.TBL aou_genphensim_niahisp_niahisp_meta_analysis.TBL
+awk 'BEGIN{FS=" "; OFS="\t"} NR==1 {print $0 "\tCHR\tPOS\tID\tIDrev"} NR>1 {split($1, values, "-"); \
+  $16 = values[1]; $17 = values[2]; $18 = $16 "-" $17 "-" toupper($2) "-" toupper($3); \
+  $19 = $16 "-" $17 "-" toupper($3) "-" toupper($2); $20 = $16 "-" $17; print $0}' \
+  aou_genphensim_niahisp_niahisp_meta_analysis.TBL > aou_genphensim_niahisp_niahisp_meta_analysis_chrposrefalt_cols.TBL
+awk 'NR==1 || $10 <= 0.05 {print}' aou_genphensim_niahisp_niahisp_meta_analysis_chrposrefalt_cols.TBL > \
+  aou_genphensim_niahisp_niahisp_meta_analysis_p_5e-2.TBL
+
