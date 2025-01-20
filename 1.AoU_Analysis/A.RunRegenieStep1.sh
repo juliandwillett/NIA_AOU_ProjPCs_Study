16e366463adb7f8d43218fa773e8afaa73147aaf
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2025-01-20 08:55:46 -0500

    Update and rename C.RunRegenieStep1.sh to A.RunRegenieStep1.sh

--- 1.AoU_Analysis/A.RunRegenieStep1.sh
+++ 1.AoU_Analysis/A.RunRegenieStep1.sh
@@ -1,20 +1,14 @@
-# Do HISP first
 awk 'NR==1 {print "#FID\tIID\tSEX"} NR>1 {print "0\t" $1 "\t" "NA"}' array_data/arrays_autosomes_post_qc_pruned_common.psam > tmp ;\
 mv tmp array_data/arrays_autosomes_post_qc_pruned_common.psam ;\
 
-groups=(hisp)
 ../regenie_v3.2.8.gz_x86_64_Linux \
     --step 1 \
     --pgen array_data/arrays_autosomes_post_qc_pruned_common \
     --phenoFile ../regenie_pheno.txt \
-    --covarFile regenie_covar_hisp.txt \
+    --covarFile regenie_covar.txt \
     --bt \
-    --out rg_step1/rg_step1_${groups[0]} \
+    --out rg_step1/rg_step1 \
     --bsize 1000 \
     --lowmem \
-    --lowmem-prefix tmp_rg_20_${groups[0]} \
-    --phenoCol AD_any \
-    --keep hisp_3sd_ids.txt
-# 1695 cases and 54767 controls for HISP with 3 SDs
-# 'AD_any': 9595 cases and 178680 controls for NON HISP with 3 SDs
-# Only 38 ICD AD cases for HISP, so not done.
+    --lowmem-prefix tmp_rg_20 \
+    --phenoCol AD_any 
