126a2e72c22ff5e16a894330694778f00da550a8
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2025-01-20 08:58:03 -0500

    Update and rename G.ProjectNIAGADS.sh to A.PrepareProjectNIAGADS.sh

--- 1.AoU_Analysis/A.PrepareProjectNIAGADS.sh
+++ 1.AoU_Analysis/A.PrepareProjectNIAGADS.sh
@@ -1,12 +1,9 @@
 # Move PIEZO2 pgen files from FASRC onto AoU: piezo2_work/NIAGADS/NIAGADS_PIEZO2_PLINK
 
-# Take array data for projection, isolate HISP folks for testing (use full data for actual analysis to maximize case count)
+# Take array data for projection
 ./plink2 --pfile array_data/arrays_allchr --geno 0.1 --hwe 1e-15 --chr 1-22 --maf 0.01 \
    --make-pgen --out array_data/aou_all_array_maf_hwe_geno --set-all-var-ids @:#:\$r:\$a \
-   --new-id-max-allele-len 10000 --keep regenie_input/regenie_covar_fid_iid_only.txt
-#./plink2 --pfile array_data/arrays_allchr \#--keep piezo2_work/rg_input/hisp_ids_3sd.txt --maf 0.01 \
-   #--geno 0.1 --hwe 1e-15 --chr 1-22 --maf 0.01 \
-   #--make-pgen --out array_data/aou_all_array_maf_hwe_geno --set-all-var-ids @:#:\$r:\$a --new-id-max-allele-len 10000 
+   --new-id-max-allele-len 10000 
    
 # Convert to BED for R package, isolating shared hits to save memory/time
 ./plink2 --pfile array_data/aou_all_array_maf_hwe_geno --extract piezo2_projection/shared_IDs_aou_all.txt \
